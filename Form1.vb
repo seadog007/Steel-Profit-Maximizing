15a6f5dbd74fb8091ef94f31e47b2c84e5520523
海豹 <007seadog@gmail.com>
2016-11-06 00:48:08 +0800

    Finished the combination part, and changed the adding to list function

--- Form1.vb
+++ Form1.vb
@@ -1,13 +1,21 @@
 ﻿Public Class Form1
+    Dim combination_element()() As Integer
     Private Sub Calculate_Click(sender As Object, e As EventArgs) Handles Calculate.Click
-
+        ReDim combination_element(SpecList.Items.Count - 1)
+        For i As Integer = 0 To SpecList.Items.Count - 1
+            ReDim combination_element(i)(Int(Total_Length.Value / SpecList.Items(i).ToString) + 1)
+            For j As Integer = 0 To Int(Total_Length.Value / SpecList.Items(i).ToString) + 1
+                combination_element(i)(j) = j
+            Next
+        Next
+        MsgBox(CartesianProduct(combination_element).Length)
     End Sub
 
     Private Sub AddSpec_Click(sender As Object, e As EventArgs) Handles AddSpec.Click
         If SpecList.Items.Contains(SpecLength.Value) Then
             MessageBox.Show("該規格已存在", "錯誤")
         Else
-            SpecList.Items.Add(SpecLength.Value)
+            SpecList.Items.Add(FormatNumber(CDbl(SpecLength.Value), 1))
         End If
     End Sub
 
@@ -20,4 +28,21 @@
     Private Sub ClearSpec_Click(sender As Object, e As EventArgs) Handles ClearSpec.Click
         SpecList.Items.Clear()
     End Sub
+
+    Private Sub SpecLength_KeyDown(sender As Object, e As KeyEventArgs) Handles SpecLength.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            AddSpec_Click(sender, e)
+        End If
+    End Sub
+
+    Private Function CartesianProduct(Of T)(ParamArray sequences As T()()) As T()()
+        Dim result As IEnumerable(Of T()) = {New T() {}}
+        For Each sequence In sequences
+            Dim s = sequence
+            result = From seq In result
+                     From item In s
+                     Select seq.Concat({item}).ToArray()
+        Next
+        Return result.ToArray()
+    End Function
 End Class
